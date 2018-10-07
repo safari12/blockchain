3463cecd18fe59610942019fcd59c7334f89c102
safari <rsafari.s@gmail.com>
2018-10-06 21:52:55 -0500

    added unit testing for all_blocks

--- test/blockchain_test.exs
+++ test/blockchain_test.exs
@@ -2,11 +2,11 @@ defmodule BlockchainTest do
   use ExUnit.Case, async: true
   doctest Blockchain
 
-  describe "latest_block" do
+  setup do
+    {:ok, %{chain: Blockchain.new}}
+  end
 
-    setup do
-      {:ok, %{chain: Blockchain.new}}
-    end
+  describe "latest_block" do
 
     test "should return nil when chain is empty", %{chain: chain} do
       assert Blockchain.latest_block(chain) == nil
@@ -19,6 +19,32 @@ defmodule BlockchainTest do
       |> Blockchain.latest_block
 
       assert latest_block == new_block
+    end
+
+  end
+
+  describe "all_blocks" do
+
+    test "should return empty array when chain is empty", %{chain: chain} do
+      assert Blockchain.all_blocks(chain) == []
+    end
+
+    test "should return all blocks for chain", %{chain: chain} do
+      1..10
+      |> Enum.each(fn i ->
+        b = Blockchain.generate_next_block(chain, "hello#{i}")
+        Blockchain.add_block(chain, b)
+      end)
+
+      blocks = Blockchain.all_blocks(chain) |> Enum.reverse
+
+      1..10
+      |> Enum.each(fn n ->
+        index = n - 1
+        b = Enum.at(blocks, index)
+
+        assert b.header.index == index
+      end)
     end
 
   end
